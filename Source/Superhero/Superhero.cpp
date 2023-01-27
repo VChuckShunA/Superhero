fafbf5c6a134a7a083058c068424b07c906a3187
Vichakshana Arangala <vichakshana.arangala@gmail.ocm>
2023-01-27 00:37:02 +0000

    Initial Commit

---
+++ Source/Superhero/Superhero.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Superhero.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Superhero, "Superhero" );
---
+++ Source/Superhero/SuperheroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperheroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERHERO_API ASuperheroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
